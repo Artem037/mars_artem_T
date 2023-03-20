120ba15644e80383befca72d3fc617d91d2a7722
Artem037 <artemysttr@gmail.com>
2023-03-20 13:22:54 +0400

    задача "Список профессий"

--- main.py
+++ main.py
@@ -15,5 +15,29 @@ def training(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    profs = ['инженер-исследователь',
+             'пилот',
+             'строитель',
+             'экзобиолог',
+             'врач',
+             'инженер по терраформированию',
+             'климатолог',
+             'специалист по радиационной защите',
+             'астрогеолог',
+             'гляциолог',
+             'инженер жизнеобеспечения',
+             'метеоролог',
+             'оператор марсохода',
+             'киберинженер',
+             'штурман',
+             'пилот дронов']
+    if list == 'ol':
+        profs = enumerate(profs, start=1)
+    return render_template('list_prof.html', list=list, profs=profs)
+
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
