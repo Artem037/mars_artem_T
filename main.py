c9675dbbadcfa0bc7fe38953fe7f13ec56c4e950
Artem037 <artemysttr@gmail.com>
2023-03-21 12:36:10 +0400

    задача "Готовимся к миссии"

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('base.html', title=title)
+    return render_template('index.html', title=title)
 
 
 @app.route('/training/<prof>')
