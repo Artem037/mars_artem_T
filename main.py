8904c604b1b0694d1d23ed180530e75cf5b3f0e3
Artem037 <artemysttr@gmail.com>
2023-03-21 23:39:28 +0400

    задача "Цвет каюты"

--- main.py
+++ main.py
@@ -66,5 +66,10 @@ def distribution():
     return render_template('/distribution.html', astronauts=astronauts)
 
 
+@app.route('/table/<sex>/<age>')
+def table(sex, age):
+    return render_template('table.html', sex=sex, age=int(age))
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
