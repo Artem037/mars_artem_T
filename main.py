c659a03959cbc13a7723966b59a9155960c53217
Artem037 <artemysttr@gmail.com>
2023-03-21 13:01:12 +0400

    задача "Готовимся к миссии"

--- main.py
+++ main.py
@@ -36,6 +36,19 @@ def list_prof(list):
     return render_template('list_prof.html', list=list, profs=profs)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    data = {'title': 'Анкета',
+            'surname': 'Watny',
+            'name': 'Mark',
+            'education': 'выше среднего',
+            'profession': 'штурман марсохода',
+            'sex': 'male',
+            'motivation': 'Всегда мечтал застрять на Марсе!',
+            'ready': 'True'}
+    return render_template('auto_answer.html', data=data)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
